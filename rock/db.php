c01da407d6ba9cd363128e6604a8188f946bfcc0
Camilo Sanchez Aguilera <csancheza@uqvirtual.edu.co>
2021-04-22 22:56:09 -0500

    Añadir funcionalidad loggin para los empleados

---
+++ rock/db.php
@@ -0,0 +1,6 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","","reparatodo");
+
+?>
+<H1>CONECTADO</H1>
---
+++ rock/pedido.php
@@ -0,0 +1,46 @@
+<?php
+include "db-conexion.php";
+
+
+$resena=$_POST['resena'];
+
+$libro=$_POST['libro'];
+echo $libro;
+
+
+$conexion=mysqli_connect("localhost","root","","bibliote");
+
+$consulta="SELECT*FROM libro where isbn='$libro'" ;
+$resultado=mysqli_query($conexion,$consulta);
+
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    $sql= "INSERT INTO rese (texto,isbn) VALUES ('$resena','$libro')";   
+
+
+    if ($link-> query($sql) === TRUE) {
+     echo "OK";      
+    }else {
+     echo "ERROR";
+    }
+    mysqli_close($link);  
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+
+
+
+
+
+?>
+<h1 class="bad">ERROR DE AUTENTIFICACION</h1>
